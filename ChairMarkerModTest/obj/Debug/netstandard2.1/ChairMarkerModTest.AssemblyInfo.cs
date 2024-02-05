f0eb51c7c21d4f8b859752fdbbcb6e5e6db87cd0
Baby Potato <jlambn@shaw.ca>
2024-02-04 21:21:12 -0800

    Add example enemyAI

--- ChairMarkerModTest/obj/Debug/netstandard2.1/ChairMarkerModTest.AssemblyInfo.cs
+++ ChairMarkerModTest/obj/Debug/netstandard2.1/ChairMarkerModTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChairMarkerModTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+072133d3ff190b5c79a892ab6add3ccce93084bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChairMarkerModTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChairMarkerModTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
