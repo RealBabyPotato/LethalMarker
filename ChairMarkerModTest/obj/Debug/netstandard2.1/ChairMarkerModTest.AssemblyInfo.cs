45680ca230fdf9e651876f3dc6eb968a42f341ef
Baby Potato <jlambn@shaw.ca>
2024-02-07 08:10:38 -0800

    add example code

--- ChairMarkerModTest/obj/Debug/netstandard2.1/ChairMarkerModTest.AssemblyInfo.cs
+++ ChairMarkerModTest/obj/Debug/netstandard2.1/ChairMarkerModTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChairMarkerModTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b76d5782c76dde401b72c6b3e892762930f5c1ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChairMarkerModTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChairMarkerModTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
